a6b28bf77cc8695bf54e00b007460bb835a744c8
JunHyun <junhyun1001@gmail.com>
2023-11-28 02:45:06 +0900

    공통 응답(성공, 실패) API 작성

---
+++ src/main/java/umc/spring/apipayload/code/status/ErrorStatus.java
@@ -0,0 +1,29 @@
+package umc.spring.apipayload.code.status;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import umc.spring.apipayload.code.BaseCode;
+import umc.spring.apipayload.code.ReasonDto;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorStatus implements BaseCode {
+
+    // 일반적인 에러 응답
+    INTERNER_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ReasonDto getReason() {
+        return ReasonDto.builder()
+                .isSuccess(false)
+                .httpStatus(httpStatus)
+                .code(code)
+                .message(message)
+                .build();
+    }
+}
