ea8770b772bce079f00cfa96552db1fe8e5233a9
JunHyun <junhyun1001@gmail.com>
2023-11-27 22:40:14 +0900

    기본 엔티티 작성

--- src/main/java/umc/spring/domain/ReviewImage.java
+++ src/main/java/umc/spring/domain/ReviewImage.java
@@ -1,2 +1,18 @@
-package umc.spring.domain;public class ReviewImage {
+package umc.spring.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import umc.spring.domain.common.BaseEntity;
+
+@Entity
+public class ReviewImage extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String imageUrl;
+
 }
--- src/main/java/umc/spring/domain/mapping/MemberAgree.java
+++ src/main/java/umc/spring/domain/mapping/MemberAgree.java
@@ -1,2 +1,16 @@
-package umc.spring.domain.mapping;public class MemberAgree {
+package umc.spring.domain.mapping;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import umc.spring.domain.common.BaseEntity;
+
+@Entity
+public class MemberAgree extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
