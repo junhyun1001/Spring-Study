bb8ba95fbde15eccbab6b9a3146bc0c62b2869fa
JunHyun <junhyun1001@gmail.com>
2023-11-28 19:39:19 +0900

    temp/exception api 테스트

--- src/main/java/umc/spring/controller/TempController.java
+++ src/main/java/umc/spring/controller/TempController.java
@@ -1,19 +1,31 @@
 package umc.spring.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import umc.spring.apipayload.ApiResponse;
 import umc.spring.converter.TempConverter;
 import umc.spring.dto.TempResponse;
+import umc.spring.service.temp.TempQueryService;
 
 @RestController
 @RequestMapping("/temp")
+@RequiredArgsConstructor
 public class TempController {
+
+    private final TempQueryService tempQueryService;
 
     @GetMapping("/test")
     public ApiResponse<TempResponse.TempTestDto> testApi() {
         return ApiResponse.onSuccess(TempConverter.toTempTestDto());
+    }
+
+    @GetMapping("/exception")
+    public ApiResponse<TempResponse.TempExceptionDto> exceptionApi(@RequestParam int flag) {
+        tempQueryService.checkFlag(flag);
+        return ApiResponse.onSuccess(TempConverter.toTempExceptionDto(flag));
     }
 
 }
