554c44456aa9903a280d3b3b86833f0a52b4bb54
JunHyun <junhyun1001@gmail.com>
2023-12-01 23:08:52 +0900

    JoinRequestDto Validation with Custom Annotation

--- src/main/java/umc/spring/dto/member/MemberRequest.java
+++ src/main/java/umc/spring/dto/member/MemberRequest.java
@@ -1,6 +1,12 @@
 package umc.spring.dto.member;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import umc.spring.domain.enums.Gender;
+import umc.spring.validation.annotation.ExistCategories;
 
 import java.util.List;
 
@@ -8,14 +14,23 @@ public class MemberRequest {
 
     @Getter
     public static class JoinRequestDto {
+        @NotBlank
         private String name;
+        @Email
         private String email;
+        @NotNull
         private String gender;
+        @NotNull
         private Integer birthYear;
+        @NotNull
         private Integer birthMonth;
+        @NotNull
         private Integer birthDay;
+        @Size(min = 5, max = 12)
         private String address;
+        @Size(min = 5, max = 12)
         private String specAddress;
+        @ExistCategories
         private List<Long> preferCategory;
     }
 
