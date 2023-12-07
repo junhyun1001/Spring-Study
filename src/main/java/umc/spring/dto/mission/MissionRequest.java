8a8667dab195f9fd0bed7c2c033aebf5b04f30a8
JunHyun <junhyun1001@gmail.com>
2023-12-07 16:10:03 +0900

    @DateTimeFormat pattern 변경

--- src/main/java/umc/spring/dto/mission/MissionRequest.java
+++ src/main/java/umc/spring/dto/mission/MissionRequest.java
@@ -12,7 +12,7 @@ public class MissionRequest {
 
         private int reward; // 미션 수행시 받을 수 있는 포인트
 
-        @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         private LocalDateTime deadline; // 미션 마감기한
 
         private String missionSpec; // 미션 내용
